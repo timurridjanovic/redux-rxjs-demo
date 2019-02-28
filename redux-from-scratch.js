1b1d20fe8b10c04c535d651b8e32c77c26577f4f
timurridjanovic <timur.ridjanovic@gmail.com>
2019-02-28 11:00:20 -0500

    add replay capability

--- redux-from-scratch.js
+++ redux-from-scratch.js
@@ -3,9 +3,19 @@
 function createStore (rootReducer) {
   const store = {}
   let callback = () => {}
+  const actions = []
+  let currentAction = actions.length - 1
+  const replay = () => {
+    const newActions = actions.slice(0, currentAction + 1)
+    store.store = newActions.reduce((state, action) => {
+      return rootReducer(state, action)
+    }, {})
+  }
   store.store = {}
   store.dispatch = action => {
-    rootReducer(store.store, action)
+    actions.push(action)
+    currentAction = Math.min(currentAction + 1, actions.length - 1)
+    store.store = rootReducer(store.store, action)
     callback()
   }
 
@@ -15,6 +25,18 @@ function createStore (rootReducer) {
 
   store.getState = () => {
     return store.store
+  }
+
+  store.undo = () => {
+    currentAction = Math.max(currentAction - 1, -1)
+    replay()
+    callback()
+  }
+
+  store.redo = () => {
+    currentAction = Math.min(currentAction + 1, actions.length - 1)
+    replay()
+    callback()
   }
 
   return store
@@ -74,9 +96,22 @@ store.dispatch({
 })
 
 store.dispatch({
+  type: ADD_VIDEO,
+  video: {
+    name: 'video1',
+    url: 'http://www.videos.com/video1'
+  }
+})
+
+store.dispatch({
   type: ADD_IMAGE,
   video: {
     name: 'image1',
     url: 'http://www.images.com/image1'
   }
 })
+
+
+store.undo()
+
+store.redo()
